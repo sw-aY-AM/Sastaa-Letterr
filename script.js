ef389dad21d6c0253ed66e6e819555804eb0278d
sw-aY-AM <dhidby@gmail.com>
2024-12-06 13:26:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     event.preventDefault(); // Prevent form submission
 
     const password = document.getElementById('password').value;
-    const correctPassword = 'devika8'; // Replace with your password
+    const correctPassword = 'devika8';
 
     if (password === correctPassword) {
         window.location.href = 'main.html'; // Redirect to main.html if password is correct
